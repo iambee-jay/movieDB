0596988346f893480fac9167a3beaea6011dfd33
Bolaji Ayodele <ayodelej30@gmail.com>
2024-01-13 16:03:22 -0700

    clean up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import SearchMovie from "./searchMovie";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="title"> Movie Search</h1>
+        <SearchMovie />
+      </div>
+    </>
+  );
+}
+
+export default App;
